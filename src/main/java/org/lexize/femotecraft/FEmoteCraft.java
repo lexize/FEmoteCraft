08825c2d1491ef2216e262dc3ba18eb08f051e9b
Lexize <tennoblyat@gmail.com>
2022-10-14 06:53:11 +0300

    Some changes that not too important... But ye

--- src/main/java/org/lexize/femotecraft/FEmoteCraft.java
+++ src/main/java/org/lexize/femotecraft/FEmoteCraft.java
@@ -40,38 +40,61 @@ public class FEmoteCraft implements FiguraAPI {
     }
     @LuaWhitelist
     public boolean isPartBendable(@LuaNotNil String modelPart) {
-        return _emotecraftPlayer.getEmote().getData().getPart(modelPart).isBendable;
+        return isEmotePlaying() && _emotecraftPlayer.getEmote().getData().getPart(modelPart).isBendable;
     }
     @LuaWhitelist
-    public FiguraVec3 getPartBend(@LuaNotNil String modelPart, Float tickDelta) {
-        if (tickDelta == null) tickDelta = 0f;
-        KeyframeAnimation.StateCollection part = _emotecraftPlayer.getEmote().getData().getPart(modelPart);
-        Vec3f v;
-        if (part.isBendable) {
-            v = new Vec3f(part.bendDirection.defaultValue, part.bend.defaultValue, 0);
-            return getTransform(modelPart, TransformType.BEND, v, tickDelta);
+    public boolean isPartEnabled(@LuaNotNil String modelPart) {
+        return isEmotePlaying() && _emotecraftPlayer.getEmote().getData().getPart(modelPart).isEnabled();
+    }
+    @LuaWhitelist
+    public FiguraVec2 getPartBend(@LuaNotNil String modelPart, Float tickDelta) {
+        if (isEmotePlaying()) {
+            if (tickDelta == null) tickDelta = 0f;
+            KeyframeAnimation.StateCollection part = _emotecraftPlayer.getEmote().getData().getPart(modelPart);
+            if (part.isEnabled()) {
+                Vec3f v;
+                if (part.isBendable) {
+                    v = new Vec3f(part.bendDirection.defaultValue, part.bend.defaultValue, 0);
+                    FiguraVec3 fv = getTransform(modelPart, TransformType.BEND, v, tickDelta);
+                    return FiguraVec2.of(fv.x, fv.y);
+                }
+            }
         }
-        return new FiguraVec3();
+        return FiguraVec2.of();
     }
     @LuaWhitelist
     public FiguraVec3 getPartPosition(@LuaNotNil String modelPart, Float tickDelta) {
-        if (tickDelta == null) tickDelta = 0f;
-        KeyframeAnimation.StateCollection part = _emotecraftPlayer.getEmote().getData().getPart(modelPart);
-        Vec3f v;
-        v = new Vec3f(part.x.defaultValue, part.y.defaultValue, part.z.defaultValue);
-        return getTransform(modelPart, TransformType.POSITION, v, tickDelta);
+        if (isEmotePlaying()) {
+            if (tickDelta == null) tickDelta = 0f;
+            KeyframeAnimation.StateCollection part = _emotecraftPlayer.getEmote().getData().getPart(modelPart);
+            if (part.isEnabled()) {
+                Vec3f v;
+                v = new Vec3f(part.x.defaultValue, part.y.defaultValue, part.z.defaultValue);
+                return getTransform(modelPart, TransformType.POSITION, v, tickDelta);
+            }
+        }
+        return FiguraVec3.of();
     }
     @LuaWhitelist
     public FiguraVec3 getPartRotation(@LuaNotNil String modelPart, Float tickDelta) {
-        if (tickDelta == null) tickDelta = 0f;
-        KeyframeAnimation.StateCollection part = _emotecraftPlayer.getEmote().getData().getPart(modelPart);
-        Vec3f v;
-        v = new Vec3f(part.pitch.defaultValue, part.yaw.defaultValue, part.roll.defaultValue);
-        return getTransform(modelPart, TransformType.ROTATION, v, tickDelta);
+        if (isEmotePlaying()) {
+            if (tickDelta == null) tickDelta = 0f;
+            KeyframeAnimation.StateCollection part = _emotecraftPlayer.getEmote().getData().getPart(modelPart);
+            if (part.isEnabled()) {
+                Vec3f v;
+                v = new Vec3f(part.pitch.defaultValue, part.yaw.defaultValue, part.roll.defaultValue);
+                return getTransform(modelPart, TransformType.ROTATION, v, tickDelta);
+            }
+        }
+        return FiguraVec3.of();
     }
     @LuaWhitelist
     public int getEmoteTick() {
-        return _emotecraftPlayer.getEmote().getTick();
+        return isEmotePlaying() ? _emotecraftPlayer.getEmote().getTick() : 0;
+    }
+    @LuaWhitelist
+    public void preparePlayerEntity() {
+        _emotecraftPlayer = _emoteCraftGetters.getPlayerFromUUID(_avatar.owner);
     }
 
     private FiguraVec3 getTransform(String modelPart, TransformType type, Vec3f v0, float tickDelta) {
@@ -82,8 +105,11 @@ public class FEmoteCraft implements FiguraAPI {
 
     @LuaWhitelist
     public List<String> getPartList() {
-        _emotecraftPlayer = _emoteCraftGetters.getPlayerFromUUID(_avatar.owner);
-        return _emotecraftPlayer.getEmote().getData().getBodyParts().keySet().stream().toList();
+        if (isEmotePlaying()) {
+            _emotecraftPlayer = _emoteCraftGetters.getPlayerFromUUID(_avatar.owner);
+            return _emotecraftPlayer.getEmote().getData().getBodyParts().keySet().stream().toList();
+        }
+        return List.of();
     }
 
 
@@ -95,7 +121,7 @@ public class FEmoteCraft implements FiguraAPI {
         _avatar = avatar;
         _emoteCraftInstance = EmoteInstance.instance;
         _emoteCraftGetters = _emoteCraftInstance.getGetters();
-        _emotecraftPlayer = _emoteCraftGetters.getPlayerFromUUID(_avatar.owner);
+        preparePlayerEntity();
     }
 
     @Override
